208114775068e5b8d4eeab3d1ffbf5050d83aadf
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-02 16:45:31 +0530

    add data access layer which are contains the data about the accessing the data from the database

---
+++ DemoPractical.DataAccessLayer/Data/ApplicationDataContext.cs
@@ -0,0 +1,30 @@
+﻿using DemoPractical.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoPractical.DataAccessLayer.Data
+{
+	public class ApplicationDataContext : DbContext
+	{
+		public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Employee> Employees { get; set; }
+
+		public DbSet<Department> Departments { get; set; }
+
+		public DbSet<EmployeeType> EmployeeTypes { get; set; }
+
+		public DbSet<PermentEmployee> PermentEmployees { get; set; }
+
+		public DbSet<ConractBaseEmployee> ConractBaseEmployees { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<ConractBaseEmployee>().HasKey(x => x.EmployeeID);
+			modelBuilder.Entity<PermentEmployee>().HasKey(x => x.EmployeeId);
+		}
+
+	}
+}
