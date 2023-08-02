77853ce31192140322e11ef8ae1ce67014435f39
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-02 18:17:24 +0530

    add domain layer

---
+++ DemoPractical.Domain/Interface/IDepartmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using DemoPractical.Models.Models;
+
+namespace DemoPractical.Domain.Interface
+{
+	public interface IDepartmentRepository
+	{
+
+		// Get all the department
+		Task<IEnumerable<Department>> GetDepartmentsAsync();
+
+		// Get the Department name by id
+		Task<Department> GetDepartmentByIdAsync(int id);
+
+		// Edit the Department 
+		Task EditDepartment(Department department);
+
+		// Delete Department by id
+		Task DeleteDepartment(int depId);
+
+		Task AddDepartment(Department department);
+	}
+}
---
+++ DemoPractical.Domain/Interface/IEmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using DemoPractical.Models.Models;
+
+namespace DemoPractical.Domain.Interface
+{
+	public interface IEmployeeRepository
+	{
+		Task<IEnumerable<Employee>> GetEmployeesAsync();
+
+		Task<Employee> GetEmployeeById(int id);
+
+		Task EditEmployee(Employee employee);
+
+		Task DeleteEmployee(Employee employee);
+
+		Task CreateEmployee(Employee employee);
+
+		Task<Department> GetEmployeeDepartment(int empId);
+
+		Task ChangeEmployeeDepartment(int empId, int depId);
+	}
+}
