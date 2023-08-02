6ad4a94e234a248ac7d7965310b84b374c40b8e8
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-02 19:07:25 +0530

    last commit

---
+++ DemoPractical.DataAccessLayer/Repositories/DepartmentRepository.cs
@@ -0,0 +1,54 @@
+﻿using DemoPractical.DataAccessLayer.Data;
+using DemoPractical.Domain.Interface;
+using DemoPractical.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoPractical.DataAccessLayer.Repositories
+{
+	public class DepartmentRepository : IDepartmentRepository
+	{
+		private readonly ApplicationDataContext _db;
+
+		public DepartmentRepository(ApplicationDataContext db)
+		{
+			_db = db;
+		}
+
+		public async Task AddDepartment(Department department)
+		{
+			await _db.Departments.AddAsync(department);
+			await _db.SaveChangesAsync();
+		}
+
+		public async Task DeleteDepartment(int depId)
+		{
+			Department dep = await GetDepartmentByIdAsync(depId);
+
+			if (dep != null)
+			{
+				_db.Departments.Remove(dep);
+				await _db.SaveChangesAsync();
+			}
+
+		}
+
+		public async Task EditDepartment(Department department)
+		{
+			_db.Update(department);
+			await _db.SaveChangesAsync();
+		}
+
+		public async Task<Department> GetDepartmentByIdAsync(int id)
+		{
+			Department? department = await _db.Departments.FirstOrDefaultAsync(x => x.Id == id);
+			return department;
+
+		}
+
+		public async Task<IEnumerable<Department>> GetDepartmentsAsync()
+		{
+			var departments = await _db.Departments.ToListAsync();
+			return departments;
+		}
+	}
+}
---
+++ DemoPractical.DataAccessLayer/Repositories/EmployeeRepository.cs
@@ -0,0 +1,64 @@
+﻿using DemoPractical.DataAccessLayer.Data;
+using DemoPractical.Domain.Interface;
+using DemoPractical.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoPractical.DataAccessLayer.Repositories
+{
+	public class EmployeeRepository : IEmployeeRepository
+	{
+		private readonly ApplicationDataContext _db;
+		private readonly IDepartmentRepository _departmentRepository;
+
+		public EmployeeRepository(ApplicationDataContext db, IDepartmentRepository departmentRepository)
+		{
+			_db = db;
+			_departmentRepository = departmentRepository;
+		}
+
+		public Task ChangeEmployeeDepartment(int empId, int depId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task CreateEmployee(Employee employee)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteEmployee(Employee employee)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task EditEmployee(Employee employee)
+		{
+			_db.Update<Employee>(employee);
+			await _db.SaveChangesAsync();
+		}
+
+		public async Task<Employee> GetEmployeeById(int id)
+		{
+			Employee employee = await _db.Employees.FirstOrDefaultAsync(x => x.Id == id);
+			return employee;
+		}
+
+		public async Task<Department> GetEmployeeDepartment(int empId)
+		{
+			Employee emp = await GetEmployeeById(empId);
+			if (emp == null || emp.DepartmentId == null)
+			{
+				return null;
+			}
+
+			Department department = await _departmentRepository.GetDepartmentByIdAsync(emp.DepartmentId ?? 1);
+			return department;
+		}
+
+		public async Task<IEnumerable<Employee>> GetEmployeesAsync()
+		{
+			var employees = await _db.Employees.ToListAsync();
+			return employees;
+		}
+	}
+}
