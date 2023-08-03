14ebd8935dd6657bd375b156d4f1b06f2da0840b
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-03 18:25:24 +0530

    add : mapping class

---
+++ DemoPractical.Models/Mappers/MappingModels.cs
@@ -0,0 +1,27 @@
+﻿using DemoPractical.Models.DTOs;
+using DemoPractical.Models.Models;
+
+namespace DemoPractical.Models.Mappers
+{
+	public static class MappingModels
+	{
+		public static EmployeeDetailsDTO MapEmployeeToDTO(Employee employee)
+		{
+			return new EmployeeDetailsDTO()
+			{
+				Email = employee.Email,
+				Name = employee.Name,
+				PhoneNumber = employee.PhoneNumber,
+			};
+		}
+
+		public static Employee MapEmployeeDTOToEmployee(EmployeeDetailsDTO dto, Employee employee)
+		{
+			employee.Name = dto.Name;
+			employee.PhoneNumber = dto.PhoneNumber;
+			employee.Email = dto.Email;
+			return employee;
+		}
+
+	}
+}
