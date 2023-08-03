e83e1f66b52760666bb6341ae4bd7f02390cfc5a
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-03 18:24:55 +0530

    add : migrations

--- DemoPractical.DataAccessLayer/Migrations/20230803060515_SeedingData.Designer.cs
+++ DemoPractical.DataAccessLayer/Migrations/20230803060515_SeedingData.Designer.cs
@@ -4,6 +4,7 @@ using DemoPractical.DataAccessLayer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DemoPractical.DataAccessLayer.Migrations
 {
     [DbContext(typeof(ApplicationDataContext))]
-    partial class ApplicationDataContextModelSnapshot : ModelSnapshot
+    [Migration("20230803060515_SeedingData")]
+    partial class SeedingData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
