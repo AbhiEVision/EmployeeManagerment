e55d7ce940100c55a550bd6c41416aaefe5b781e
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-03 18:26:19 +0530

    change : updated repositories

--- DemoPractical.DataAccessLayer/Repositories/DepartmentRepository.cs
+++ DemoPractical.DataAccessLayer/Repositories/DepartmentRepository.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DemoPractical.DataAccessLayer.Repositories
 {
+	/// <summary>
+	/// Department Repository for headlining the operations of the database
+	/// </summary>
 	public class DepartmentRepository : IDepartmentRepository
 	{
 		private readonly ApplicationDataContext _db;
@@ -14,12 +17,35 @@ namespace DemoPractical.DataAccessLayer.Repositories
 			_db = db;
 		}
 
+		/// <summary>
+		/// Adding the department 
+		/// </summary>
+		/// <param name="department"></param>
 		public async Task AddDepartment(Department department)
 		{
+			department.DepartmentName = department.DepartmentName.ToUpper();
 			await _db.Departments.AddAsync(department);
 			await _db.SaveChangesAsync();
 		}
 
+		/// <summary>
+		/// Check if there is department exists or not
+		/// </summary>
+		/// <param name="depId"></param>
+		public async Task<bool> IsDepartmentExists(int depId)
+		{
+			Department dep = await _db.Departments.FirstOrDefaultAsync(x => x.Id == depId);
+			if (dep == null)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// Deleting the department
+		/// </summary>
+		/// <param name="depId"></param>
 		public async Task DeleteDepartment(int depId)
 		{
 			Department dep = await GetDepartmentByIdAsync(depId);
@@ -29,15 +55,28 @@ namespace DemoPractical.DataAccessLayer.Repositories
 				_db.Departments.Remove(dep);
 				await _db.SaveChangesAsync();
 			}
-
 		}
 
+		/// <summary>
+		/// Edit the department
+		/// </summary>
+		/// <param name="department"></param>
 		public async Task EditDepartment(Department department)
 		{
-			_db.Update(department);
+			Department existingDep = await GetDepartmentByIdAsync(department.Id);
+			if (existingDep == null)
+			{
+				return;
+			}
+			existingDep.DepartmentName = department.DepartmentName.ToUpper();
+			_db.Update(existingDep);
 			await _db.SaveChangesAsync();
 		}
 
+		/// <summary>
+		/// Get department by id
+		/// </summary>
+		/// <param name="id"></param>
 		public async Task<Department> GetDepartmentByIdAsync(int id)
 		{
 			Department? department = await _db.Departments.FirstOrDefaultAsync(x => x.Id == id);
@@ -45,6 +84,9 @@ namespace DemoPractical.DataAccessLayer.Repositories
 
 		}
 
+		/// <summary>
+		/// Get the all departments 
+		/// </summary>
 		public async Task<IEnumerable<Department>> GetDepartmentsAsync()
 		{
 			var departments = await _db.Departments.ToListAsync();
--- DemoPractical.DataAccessLayer/Repositories/EmployeeRepository.cs
+++ DemoPractical.DataAccessLayer/Repositories/EmployeeRepository.cs
@@ -1,10 +1,14 @@
 ﻿using DemoPractical.DataAccessLayer.Data;
 using DemoPractical.Domain.Interface;
+using DemoPractical.Models.DTOs;
 using DemoPractical.Models.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DemoPractical.DataAccessLayer.Repositories
 {
+	/// <summary>
+	/// Employee Repository which maintains the working with the database operations related to the employee table
+	/// </summary>
 	public class EmployeeRepository : IEmployeeRepository
 	{
 		private readonly ApplicationDataContext _db;
@@ -16,33 +20,86 @@ namespace DemoPractical.DataAccessLayer.Repositories
 			_departmentRepository = departmentRepository;
 		}
 
-		public Task ChangeEmployeeDepartment(int empId, int depId)
+		/// <summary>
+		/// Changing the department of employee
+		/// </summary>
+		/// <param name="empId">employee id for which we want to change the department</param>
+		/// <param name="depId">department id first check if there is exists or not after that we will update it!</param>
+		/// <returns>Nothing!</returns>
+		public async Task ChangeEmployeeDepartment(int empId, int depId)
 		{
-			throw new NotImplementedException();
+			bool depExists = await _departmentRepository.IsDepartmentExists(depId);
+			if (depExists)
+			{
+				Employee employee = await _db.Employees.FirstOrDefaultAsync(x => x.Id == empId);
+				if (employee == null)
+				{
+					return;
+				}
+				employee.DepartmentId = depId;
+				await _db.SaveChangesAsync();
+			}
+			return;
 		}
 
-		public Task CreateEmployee(Employee employee)
+		/// <summary>
+		/// Create employee
+		/// </summary>
+		/// <param name="employee">create this employee</param>
+		public async Task CreateEmployee(Employee employee)
 		{
-			throw new NotImplementedException();
-		}
-
-		public Task DeleteEmployee(Employee employee)
-		{
-			throw new NotImplementedException();
-		}
-
-		public async Task EditEmployee(Employee employee)
-		{
-			_db.Update<Employee>(employee);
+			await _db.Employees.AddAsync(employee);
 			await _db.SaveChangesAsync();
 		}
 
+		/// <summary>
+		/// Delete Employee using class
+		/// </summary>
+		public async Task DeleteEmployee(Employee employee)
+		{
+			Employee getEmp = await _db.Employees.FirstOrDefaultAsync(x => x.Id == employee.Id);
+			if (getEmp == null)
+			{
+				return;
+			}
+
+			_db.Remove(employee);
+			await _db.SaveChangesAsync();
+		}
+
+		/// <summary>
+		/// Edit the existing employee
+		/// </summary>
+		public async Task EditEmployeeDetails(int empId, EmployeeDetailsDTO employee)
+		{
+			Employee oldEmployee = await GetEmployeeById(empId);
+			if (oldEmployee == null)
+			{
+				return;
+			}
+			oldEmployee.Name = employee.Name;
+			oldEmployee.Email = employee.Email;
+			oldEmployee.PhoneNumber = employee.PhoneNumber;
+			_db.Update(oldEmployee);
+			await _db.SaveChangesAsync();
+		}
+
+		/// <summary>
+		/// Returns the employee using its id
+		/// </summary>
+		/// <param name="id">id for fetch</param>
+		/// <returns >Employee object from database </returns>
 		public async Task<Employee> GetEmployeeById(int id)
 		{
 			Employee employee = await _db.Employees.FirstOrDefaultAsync(x => x.Id == id);
 			return employee;
 		}
 
+		/// <summary>
+		/// Returns the department of the employee
+		/// </summary>
+		/// <param name="empId"></param>
+		/// <returns>The Department object</returns>
 		public async Task<Department> GetEmployeeDepartment(int empId)
 		{
 			Employee emp = await GetEmployeeById(empId);
@@ -55,10 +112,54 @@ namespace DemoPractical.DataAccessLayer.Repositories
 			return department;
 		}
 
-		public async Task<IEnumerable<Employee>> GetEmployeesAsync()
+		/// <summary>
+		/// Returns the all Employees
+		/// </summary>
+		/// <returns>List of Employee</returns>
+		public async Task<IEnumerable<EmployeeDetailsDTO>> GetEmployeeDetailsAsync()
 		{
 			var employees = await _db.Employees.ToListAsync();
-			return employees;
+
+			return MapListToEmployeeDetails(employees);
+		}
+
+		private List<EmployeeDetailsDTO> MapListToEmployeeDetails(List<Employee> employees)
+		{
+			var employeeDTOs = new List<EmployeeDetailsDTO>();
+
+			foreach (var employee in employees)
+			{
+				employeeDTOs.Add(new EmployeeDetailsDTO()
+				{
+					Email = employee.Email,
+					Name = employee.Name,
+					PhoneNumber = employee.PhoneNumber,
+				});
+			}
+
+			return employeeDTOs;
+
+		}
+
+		private EmployeeDetailsDTO MapEmployeeToItsDTO(Employee employee)
+		{
+			var employeeDTO = new EmployeeDetailsDTO();
+			employeeDTO.Email = employee.Email;
+			employeeDTO.Name = employee.Name;
+			employeeDTO.PhoneNumber = employee.PhoneNumber;
+
+			return employeeDTO;
+		}
+
+		public async Task<EmployeeDetailsDTO> GetEmployeeDetails(int empId)
+		{
+			Employee employee = await _db.Employees.FirstOrDefaultAsync(x => x.Id == empId);
+			if (employee == null)
+			{
+				return null;
+			}
+
+			return MapEmployeeToItsDTO(employee);
 		}
 	}
 }
