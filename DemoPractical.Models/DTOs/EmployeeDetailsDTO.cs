ba025e2d48544fb20891b20d5b11ab77424608c9
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-03 18:25:09 +0530

    add : dtos

---
+++ DemoPractical.Models/DTOs/EmployeeDetailsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoPractical.Models.DTOs
+{
+	public class EmployeeDetailsDTO
+	{
+		public string? Name { get; set; }
+
+		public string? Email { get; set; }
+
+		public string PhoneNumber { get; set; }
+	}
+}
