6021730a4390ab8d0e01c30e49a86f4897d6ebbd
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-03 18:27:59 +0530

    add : registrations of services

--- DemoPractical.API/Program.cs
+++ DemoPractical.API/Program.cs
@@ -1,24 +1,67 @@
+using DemoPractical.API.Swagger;
 using DemoPractical.DataAccessLayer.Data;
+using DemoPractical.DataAccessLayer.Repositories;
+using DemoPractical.DataAccessLayer.ValidationClass;
+using DemoPractical.Domain.Interface;
+using DemoPractical.Models.Models;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Services Register
 
-builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDataContext>(options =>
 {
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DemoApp"));
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Fluent Validation 
+builder.Services.AddFluentValidationAutoValidation();
+
+// Register class for fluent validations 
+builder.Services.AddScoped<IValidator<ConractBaseEmployee>, ContractBaseEmployeeValidate>();
+builder.Services.AddScoped<IValidator<Department>, DepartmentValidation>();
+builder.Services.AddScoped<IValidator<Employee>, EmployeeValidation>();
+builder.Services.AddScoped<IValidator<PermentEmployee>, PermanentEmployeeValidation>();
+
+// Register Repositories
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
+// Swagger options registrations
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfigurationOptions>();
+
+//// API Versing
+//builder.Services.AddApiVersioning(opt =>
+//{
+//	opt.AssumeDefaultVersionWhenUnspecified = true;
+//	opt.DefaultApiVersion = new ApiVersion(1, 0);
+//	opt.ReportApiVersions = true;
+//});
+
+////Setup API explorer that is API version aware
+//builder.Services.AddVersionedApiExplorer(setup =>
+//{
+//	setup.GroupNameFormat = "'v'VVV";
+//	setup.SubstituteApiVersionInUrl = true;
+//});
+
+#endregion
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+#region Adding Middleware
+
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
@@ -32,3 +75,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+#endregion
