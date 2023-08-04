d0ae092be90426df3beaacba71f53a7157f07d1a
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-04 17:10:04 +0530

    add : more functionalities to the mapper class

--- DemoPractical.Models/Mappers/MappingModels.cs
+++ DemoPractical.Models/Mappers/MappingModels.cs
@@ -23,5 +23,59 @@ namespace DemoPractical.Models.Mappers
 			return employee;
 		}
 
+		public static Employee MapCreateEmployeeClassToEmployeeClass(CreateEmployeeDTO dto)
+		{
+			return new Employee()
+			{
+				Name = dto.Name,
+				Email = dto.Email,
+				Password = dto.Password,
+				DepartmentId = dto.DepartmentId,
+				EmployeeTypeId = dto.EmployeeTypeId,
+				PhoneNumber = dto.PhoneNumber
+			};
+		}
+
+		public static PermentEmployee MapCeateEmployeeClassToPermanentEmployeeClass(int id, CreateEmployeeDTO dto)
+		{
+			return new PermentEmployee()
+			{
+				EmployeeId = id,
+				Salary = dto.Salary ?? 0
+			};
+		}
+
+		public static ConractBaseEmployee MapCreateEmployeeToContratBaseEmployeeClass(int id, CreateEmployeeDTO dto)
+		{
+			return new ConractBaseEmployee()
+			{
+				EmployeeID = id,
+				HourlyPaid = dto.HourlyPaid ?? 0
+			};
+		}
+
+		public static EmployeeSalaryDetails MapEmployeeSalaryDetailsAndItsPermanentEmployeeDetails(EmployeeDetailsDTO dto, PermentEmployee permentEmployee)
+		{
+			return new EmployeeSalaryDetails()
+			{
+				EmployeeName = dto.Name,
+				EmployeeEmail = dto.Email,
+				TypeOfEmployee = "Permanent",
+				Salary = permentEmployee.Salary,
+				HourlyPaid = null
+			};
+		}
+
+		public static EmployeeSalaryDetails MapEmployeeSalaryDetailsAndItsContractBaseEmployeeDetails(EmployeeDetailsDTO dto, ConractBaseEmployee conractBaseEmployee)
+		{
+			return new EmployeeSalaryDetails()
+			{
+				EmployeeName = dto.Name,
+				EmployeeEmail = dto.Email,
+				TypeOfEmployee = "Permanent",
+				HourlyPaid = conractBaseEmployee.HourlyPaid,
+				Salary = null
+			};
+		}
 	}
 }
