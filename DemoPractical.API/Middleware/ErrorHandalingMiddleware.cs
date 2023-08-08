8e8eedc7e058819183d6d13a832b57e295fe5041
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-08 14:37:52 +0530

    update : middle ware

--- DemoPractical.API/Middleware/ErrorHandalingMiddleware.cs
+++ DemoPractical.API/Middleware/ErrorHandalingMiddleware.cs
@@ -9,13 +9,13 @@ namespace DemoPractical.API.Middleware
 			try
 			{
 				await next.Invoke(context);
-
 			}
 			catch (Exception ex)
 			{
 				context.Response.StatusCode = 500;
 				await context.Response.WriteAsync(ex.Message);
 				SentrySdk.CaptureException(ex);
+				//SentrySdk.CaptureEvent(x);
 			}
 		}
 	}
